59fa1a1830f8200037b96a849aee0825feded8e1
Kleber Molina Kardel <molinakardel@gmail.com>
2025-05-21 13:52:55 -0300

    feat: cria função para renderizar os cards da página atual
    Cria função renderCards() para renderizar os cards a serem exibidos na página atual

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,36 @@
+let allCards = [];
+let currentPage = 1;
+const cardsPerPage = 20;
+
 fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
     .then(response => response.json())
     .then(data => {
-        const cardsContainer = document.getElementById('cards-container');
-        cardsContainer.innerHTML = ''; // Limpa antes de inserir
-
-
-        data.data.forEach(card => {
-            // Cria um elemento div para o card
-            const cardElement = document.createElement('div');
-            cardElement.classList.add('card');
-
-            // Insere conteúdo HTML no card
-            cardElement.innerHTML = `
-            <img src="${card.card_images[0].image_url}" alt="${card.name}" />
-            <h2>${card.name}</h2>
-            <p><strong>ATK:</strong> ${card.atk} | <strong>DEF:</strong> ${card.def}</p>
-            <p>${card.desc}</p>
-            `;
-
-            // Adiciona o card ao container
-            cardsContainer.appendChild(cardElement);
-        });
+        // Função para exibir 20 cards por página
+        function renderCards() {
+            const cardsContainer = document.getElementById('cards-container');
+            cardsContainer.innerHTML = ''; // Limpa antes de inserir
+    
+            const start = (currentPage - 1) * cardsPerPage;
+            const end = start + cardsPerPage;
+            const cardsToShow = allCards.slice(start, end);
+    
+            cardsToShow.forEach(card => {
+                // Cria um elemento div para o card
+                const cardElement = document.createElement('div');
+                cardElement.classList.add('card');
+    
+                // Insere conteúdo HTML no card
+                cardElement.innerHTML = `
+                <img src="${card.card_images[0].image_url}" alt="${card.name}" />
+                <h2>${card.name}</h2>
+                <p><strong>ATK:</strong> ${card.atk} | <strong>DEF:</strong> ${card.def}</p>
+                <p>${card.desc}</p>
+                `;
+    
+                // Adiciona o card ao container
+                cardsContainer.appendChild(cardElement);
+            });
+        }  
     })
     .catch(error => {
         console.error('Erro ao buscar dados da API:', error); // Exibe mensagem de erro caso retorne algum erro ao buscar dados da API
